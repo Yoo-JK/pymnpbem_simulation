b5a03d41f342399862625cdddaafb5ab01a3bfb1
Jaekak Yoo <yoojk1025@gmail.com>
2026-05-02 21:08:45 +0000

    Wave2 γ Step1: material/nonlocal_eps.py 정식 EpsNonlocal 호출로 교체
    EpsFun 우회 (M7 Wave 3) 제거. mnpbem.materials.EpsNonlocal /
    make_nonlocal_pair 호출하여 Yu Luo 식을 정식 캡슐화.
    
    build_nonlocal_eps(spec, eps_embed) → (eps_embed, eps_metal_core, eps_shell)
    3-튜플 반환. epstab[0]=embed, [1]=core Drude/Table, [2]=nonlocal shell.
    
    지원 metal: gold/silver/aluminum (built-in v_F),
    from_table:<path>.dat (J-C tabulated + canonical Drude params).

--- pymnpbem_simulation/material/__init__.py
+++ pymnpbem_simulation/material/__init__.py
@@ -1,10 +1,8 @@
 from .nonlocal_eps import (
         build_nonlocal_eps,
-        make_hydrodynamic_drude_eps,
         is_nonlocal_spec)
 
 
 __all__ = [
         'build_nonlocal_eps',
-        'make_hydrodynamic_drude_eps',
         'is_nonlocal_spec']
