fac332fd842591aa9b0a344d41c9ceff4232ca25
Jaekak Yoo <yoojk1025@gmail.com>
2026-05-02 07:07:22 +0000

    Phase 2 Wave 2 M3 Task 1-3: FieldCalculator + grid_builder 추가
    - simulation/field_calculator.py: MeshField wrapper. rectangular/spherical/custom 그리드 위 E,H 평가
    - simulation/grid_builder.py: make_rectangular_grid, make_spherical_grid, make_custom_points 헬퍼
    - simulation/__init__.py: FieldCalculator, grid_builder export append

--- pymnpbem_simulation/simulation/__init__.py
+++ pymnpbem_simulation/simulation/__init__.py
@@ -1,4 +1,11 @@
 from .base import SimulationRunner, run_simulation
 from .planewave_ret import PlaneWaveRetRunner
+from .field_calculator import FieldCalculator
+from . import grid_builder
 
-__all__ = ['SimulationRunner', 'run_simulation', 'PlaneWaveRetRunner']
+__all__ = [
+        'SimulationRunner',
+        'run_simulation',
+        'PlaneWaveRetRunner',
+        'FieldCalculator',
+        'grid_builder']
