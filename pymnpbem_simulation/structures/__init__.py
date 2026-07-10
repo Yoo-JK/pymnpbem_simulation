ffecf6c6733ac015b55dda6f708a8dece6ec06e1
Ryan Berwick <97850329+rberwickt@users.noreply.github.com>
2026-07-10 12:05:12 -0500

    Add support for python module descriptor materials

--- pymnpbem_simulation/structures/__init__.py
+++ pymnpbem_simulation/structures/__init__.py
@@ -1,4 +1,6 @@
 from typing import Any, Dict, Tuple
+from ..material.material_descriptor import resolve_refractive_index_paths
+import copy
 
 from .base import StructureBuilder
 from .sphere import SphereBuilder
@@ -54,7 +56,12 @@ def build_structure(cfg_struct: Dict[str, Any],
                 'available: {}'.format(stype, sorted(REGISTRY.keys())))
 
     cls = REGISTRY[stype]
-    builder = cls(cfg_struct, cfg_materials)
+    cfg_materials_local = copy.deepcopy(cfg_materials) if isinstance(cfg_materials, dict) else {}
+    ri_paths = cfg_materials_local.get("refractive_index_paths", {})
+    if isinstance(ri_paths, dict) and ri_paths:
+        cfg_materials_local["refractive_index_paths"] = resolve_refractive_index_paths(ri_paths)
+    builder = cls(cfg_struct, cfg_materials_local)
+    # builder = cls(cfg_struct, cfg_materials)
     return builder.build()
 
 
